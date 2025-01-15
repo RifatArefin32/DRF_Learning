4c0d4de1d0647250a5f480823d7d6f84f3e67035
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-15 19:40:27 +0600

    Create Order model

--- drf_app_first/models.py
+++ drf_app_first/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -33,3 +34,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+
+
+    class Order(models.Model):
+        class StatusChoices(models.TextChoices):
+            PENDING = 'Pending'
+            CONFIRMED = 'Confirmed'
+            CANCELLED = 'Cancelled'
+        
+        order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        created_at = models.DateTimeField(auto_now_add=True)
+        status = models.CharField(max_length=10, choices=StatusChoices)
