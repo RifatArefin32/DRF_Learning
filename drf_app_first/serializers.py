a858b195ee8603d7d2e3db16b3d989c6f993f2e1
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-16 17:47:41 +0600

    Add order and order item serializer class

--- drf_app_first/serializers.py
+++ drf_app_first/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Order, OrderItem
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'stock']
+        fields = ['id', 'name', 'description', 'price', 'stock', 'in_stock']
 
     def validate_price(self, value):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than zero")
         return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'product', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ['order_id', 'user', 'status', 'product', 'items']
