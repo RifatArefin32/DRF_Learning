d17c4621d6da61bbee71eecc45d78f63b378ad29
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-16 12:14:30 +0600

    Create OrderItem model

--- drf_app_first/models.py
+++ drf_app_first/models.py
@@ -36,13 +36,32 @@ class Product(models.Model):
     
 
 
-    class Order(models.Model):
-        class StatusChoices(models.TextChoices):
-            PENDING = 'Pending'
-            CONFIRMED = 'Confirmed'
-            CANCELLED = 'Cancelled'
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
         
-        order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-        user = models.ForeignKey(User, on_delete=models.CASCADE)
-        created_at = models.DateTimeField(auto_now_add=True)
-        status = models.CharField(max_length=10, choices=StatusChoices)
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)   
+    status = models.CharField(max_length=10, choices=StatusChoices.choices, default=StatusChoices.PENDING)
+    product = models.ManyToManyField(Product, through="OrderItem" related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.price} in order {self.order.order_id}"
+    
