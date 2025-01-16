51071895291c30f2b1dabef14f1a23efc75106f2
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-16 17:24:10 +0600

    Show product_list in api_view method

--- drf_app_first/views.py
+++ drf_app_first/views.py
@@ -1,12 +1,16 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Product
 from .serializers import ProductSerializer
 
+
 # Create your views here.
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
 
-    return JsonResponse({
+    return Response({
         "data": serializer.data
     })
