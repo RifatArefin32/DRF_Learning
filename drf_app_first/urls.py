b9493d63f51af296bb592cfb7d6ce6de5d1d8a6a
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-20 17:51:10 +0600

    Add ProductListCreate API view

--- drf_app_first/urls.py
+++ drf_app_first/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from .views import ProductListAPIView, ProductDetailAPIView, OrderListAPIView, UserOrderListAPIView, ProductInfoAPIView
+from .views import ProductListAPIView, ProductDetailAPIView, OrderListAPIView, UserOrderListAPIView, ProductInfoAPIView, ProductListCreateAPIView
 
 urlpatterns = [
     # path('products/', product_list, name='product_list'),
-    path('products/', ProductListAPIView.as_view(), name='product_list'),
-    #path('products/info/', product_info, name='product_info'),
-    path('products/info/', ProductInfoAPIView.as_view(), name='product_info'),
+    # path('products/info/', product_info, name='product_info'),
     # path('products/<int:pk>/', product_details, name='product_details'),
-    path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product_details'),
     # path('orders/', order_list, name='order_list'),
+    # path('products/create/', ProductCreateAPIView.as_view(), name='product_create'),
+    # path('products/', ProductListAPIView.as_view(), name='product_list'),
+    
+    path('products/', ProductListCreateAPIView.as_view(), name='product_list'),
+    path('products/info/', ProductInfoAPIView.as_view(), name='product_info'),
+    path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product_details'),
     path('orders/', OrderListAPIView.as_view(), name='order_list'),
     path('user-orders/', UserOrderListAPIView.as_view(), name='user_order_list'),
 ]
--- drf_app_first/views.py
+++ drf_app_first/views.py
@@ -12,8 +12,6 @@ from .serializers import PorductInfoSerializer, ProductSerializer, OrderSerializ
 
 # Create your views here.
 
-
-
 # Function-based View to list all products
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -22,14 +20,6 @@ def product_list(request):
     serializer = ProductSerializer(products, many=True)
 
     return Response(serializer.data)
-
-
-
-# Class-based View to list all products
-class ProductListAPIView(generics.ListAPIView):
-    # queryset = Product.objects.all()
-    queryset = Product.objects.filter(stock__gt=10)
-    serializer_class = ProductSerializer
 
 
 
@@ -43,13 +33,6 @@ def product_details(request, pk):
 
 
 
-# Class-based view to show a product details 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
-
 # Function-based View to list all orders
 @api_view(['GET'])
 def order_list(request):
@@ -58,6 +41,50 @@ def order_list(request):
     serializer = OrderSerializer(orders, many=True)
 
     return Response(serializer.data)
+
+
+
+# Function-based View to list all product info
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    max_price = products.aggregate(max_price=Max('price'))['max_price']
+    serializer = PorductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': max_price
+    })
+
+    return Response(serializer.data)
+
+
+
+# Class-based View to list all products
+class ProductListAPIView(generics.ListAPIView):
+    # queryset = Product.objects.all()
+    queryset = Product.objects.filter(stock__gt=10)
+    serializer_class = ProductSerializer
+
+
+
+# Create a product
+class ProductCreateAPIView(generics.CreateAPIView):
+    model = Product
+    serializer_class = ProductSerializer
+
+
+
+# Create and list all products
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
+# Class-based view to show a product details 
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 
@@ -78,21 +105,6 @@ class UserOrderListAPIView(generics.ListAPIView):
         qs = super().get_queryset()
         user = self.request.user
         return qs.filter(user=user)
-
-
-
-# Function-based View to list all product info
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    max_price = products.aggregate(max_price=Max('price'))['max_price']
-    serializer = PorductInfoSerializer({
-        'products': products,
-        'count': len(products),
-        'max_price': max_price
-    })
-
-    return Response(serializer.data)
 
 
 
