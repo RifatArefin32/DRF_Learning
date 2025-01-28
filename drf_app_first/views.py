7788ce4b3330d507051c8cfe2457e367c3530264
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2025-01-28 17:59:23 +0600

    Customizing permission

--- drf_app_first/views.py
+++ drf_app_first/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Max
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Product, Order
@@ -78,6 +78,12 @@ class ProductCreateAPIView(generics.CreateAPIView):
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 
